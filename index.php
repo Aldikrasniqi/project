077f2a182c70f73dbc0f92013f6d28ff4aa983b0
Aldikrasniqi <aldikrasniqi5@gmail.com>
2022-06-10 15:41:35 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1></h1>
+    <div>
+        <form action="addList.php" method="POST">
+            <input type="text" placeholder="New iteam">
+            <button type="submit">Add new</button>
+        </form>
+    </div>
+    <table border="1">
+        <tr>
+            <td>Nr.</td>
+            <td>Task</td>
+            <td>Actions</td>
+            </tr>
+    </table>
+</body>
+</html>
